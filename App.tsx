c1c7a3db0d31e36ac25f66c6f968b312a5ab1404
Oleg Zaporozhets <zaporozhets93@gmail.com>
2024-11-20 15:25:32 +0200

    config firebase

--- App.tsx
+++ App.tsx
@@ -1,10 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useRef, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import firestore from "@react-native-firebase/firestore";
+import { Todo } from "./types/types";
 
 export default function App() {
+  const todosRef = useRef(firestore().collection<Todo>("todos")).current;
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const todos = [];
+      firestore()
+        .collection("todos")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((documentSnapshot) => {
+            todos.push({
+              id: documentSnapshot.id,
+              ...documentSnapshot.data(),
+            });
+          });
+        });
+    };
+
+    getTodos();
+  }, []);
+
+  const addTodoHandler = async () => {
+    const todo: Todo = {
+      id: Math.random().toString(),
+      userId: Math.random().toString(),
+      title: "New todo",
+      description: "Description todo",
+      done: false,
+    };
+
+    try {
+      console.log("clicked");
+
+      const result = await firestore().collection<Todo>("todos").add(todo);
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="add" onPress={addTodoHandler} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +58,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
